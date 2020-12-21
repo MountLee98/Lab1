239c7b72ae035841319f84583364483026a515b7
MountLee98 <michalpa98@gmail.com>
2020-12-21 16:30:01 +0100

    Add Lab1 project

---
+++ src/main/java/aplikacje/internetowe/InternetoweApplication.java
@@ -0,0 +1,13 @@
+package aplikacje.internetowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetoweApplication.class, args);
+	}
+
+}
